c1c514612a4eaba47316762caf0479665e9ed718
danie <danie@172.17.151.71>
2021-09-12 18:01:38 +0900

    Fruit App Sort

---
+++ src/com/lab2/Main.java
@@ -0,0 +1,42 @@
+package com.lab2;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Fruit> fl = new ArrayList<Fruit>();
+		System.out.println("Fruit List (ordered by name)");
+		Fruit s1 = new Fruit(101, "Apple", 1000);
+		fl.add(s1);
+		Fruit s2 = new Fruit(102, "Orange", 1500);
+		fl.add(s2);
+		fl.add(new Fruit(103, "Kiwi", 2000));
+		fl.add(new Fruit(104, "Watermelon", 2500));
+		fl.add(new Fruit(105, "Pear", 2800));
+		fl.add(new Fruit(106, "Melon", 3200));
+		fl.add(new Fruit(107, "Cherry", 1700));
+		fl.add(new Fruit(108, "Mango", 5000));
+		fl.add(new Fruit(109, "Dragon Fruit", 1250));
+		fl.add(new Fruit(110, "Papaya", 1400));
+		
+		Collections.sort(fl, new FruitComparator());
+		
+		for(Fruit s: fl) {
+			System.out.println(s.toString());
+		}
+		
+		System.out.println("Fruit List (reverse ordered by name)");
+		
+		Collections.sort(fl, new FruitComparatorDesc());
+		
+		for(int i = 0; i<fl.size(); i++) {
+			System.out.println(fl.get(i).toString());
+		}
+
+	}
+
+}
